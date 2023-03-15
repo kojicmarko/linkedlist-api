6e11edd07d892e4a5fb2ebc679dffb03cd7d614b
Marko <kojicmarko96@gmail.com>
2023-03-15 14:02:57 +0100

    Setup express-session with Redis

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,31 @@
 import express from "express";
+import session from "express-session";
+import Redis from "ioredis";
+import connectRedis from "connect-redis";
 import { unknownEndpoint } from "./middleware/index";
-require("express-async-errors");
+import { COOKIE_NAME, REDIS_URL, SECRET } from "./utils/config";
+// TODO: deal with that:
+// require("express-async-errors");
 
 const app = express();
+
+const RedisStore = connectRedis(session);
+export const redisClient = new Redis(REDIS_URL);
+
+app.use(
+  session({
+    name: COOKIE_NAME,
+    store: new RedisStore({ client: redisClient }),
+    secret: SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60, // 1 hour
+    },
+  })
+);
 
 import pingRouter from "./routes/ping";
 
--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const DATABASE_URL = process.env.DATABASE_URL;
+export const REDIS_URL = process.env.REDIS_URL as string;
 export const PORT = process.env.PORT || 3001;
-export const SECRET = process.env.SECRET;
+export const SECRET = process.env.SECRET as string;
 export const COOKIE_NAME = "sid";
